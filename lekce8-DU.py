77dae811bb0674ecf0069c934465e99b0cbd3029
mahavan <info@mahovsky.net>
2024-11-15 22:10:02 +0100

    Tasks assignment

---
+++ lekce8-DU.py
@@ -0,0 +1,35 @@
+"""
+Task 1
+You have two text files. Find out if their lines match. If they don't, print the mismatched line from each file.
+"""
+
+"""
+Task 2
+You have a text file. Create a new file and write the following statistics based on the source file to it:
+
+Number of characters;
+Number of lines;
+Number of vowels;
+Number of consonants;
+Number of digits.
+"""
+
+"""
+Task 3
+You have a text file. Delete the last line from it. Write the result to another file.
+"""
+
+"""
+Task 4
+You have a text file. Find the length of the longest line.
+"""
+
+"""
+Task 5
+You have a text file. Count how many times the word specified by the user occurs in it.
+"""
+
+"""
+Task 6
+You have a text file. Find and replace the specified word. The user determines what to search for and to what it should be replaced.
+"""
