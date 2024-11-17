1f2e2bc1c8515e6849d5e65a7fc872d21a235b33
mahavan <info@mahovsky.net>
2024-11-17 17:15:02 +0100

    Task 1 finished

--- lekce8-DU.py
+++ lekce8-DU.py
@@ -2,6 +2,25 @@
 Task 1
 You have two text files. Find out if their lines match. If they don't, print the mismatched line from each file.
 """
+def compare_text(file1, file2):
+    with open(file1, encoding="utf-8") as f1, open(file2, encoding="utf-8") as f2:
+        data_f1 = f1.readlines()
+        data_f2 = f2.readlines()
+    output = ""
+    if len(data_f1) > len(data_f2): # if file2 is shorter add empty lines
+        diff = len(data_f1) - len(data_f2)
+        for _ in range(diff):
+            data_f2.append("\n")
+    else: # if file1 is shorter add empty lines
+        diff = len(data_f2) - len(data_f1)
+        for _ in range(diff):
+            data_f1.append("\n")
+    for i in range(len(data_f1)): # different lines
+        if data_f1[i] != data_f2[i]:
+            output += data_f1[i] + data_f2[i]
+    return output
+
+print(compare_text("task1-1.txt", "task1-2.txt"))
 
 """
 Task 2
