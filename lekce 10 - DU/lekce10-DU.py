18789d2e3844937c781aa1167a6bb99b5b744ecd
mahavan <info@mahovsky.net>
2024-11-27 23:39:47 +0100

    Tasks assignment

---
+++ lekce 10 - DU/lekce10-DU.py
@@ -0,0 +1,36 @@
+"""
+Task 1
+Create a Ship class containing information about a ship.
+Use inheritance to implement a Frigate class (contains info about a frigate), a Destroyer class (contains info
+about a destroyer), a Cruiser class (contains info about a cruiser).
+Each class must have the required methods.
+"""
+
+
+"""
+Task 2
+Create a function to display the current time. The function takes no parameters. 
+Decorate the function using another function without utilizing decorator syntax. Possible output:
+
+***************************
+23:00
+***************************
+
+Two lines of asterisks here are the result of the decoration.
+"""
+
+
+"""
+Task 3
+Do Task 2 using decorator syntax.
+"""
+
+"""
+Task 4
+Create a pizza maker app. Each pizza has different ingredients. Use decorators to create different pizzas:
+Margarita;
+Four cheese;
+Capricciosa;
+Hawaiian.
+"""
+
