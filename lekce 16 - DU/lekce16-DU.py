7eb938ae3c0c0142b1ba43920a080bf22ea408b0
mahavan <info@mahovsky.net>
2025-01-29 11:03:07 +0100

    add Task 2

--- lekce 16 - DU/lekce16-DU.py
+++ lekce 16 - DU/lekce16-DU.py
@@ -4,6 +4,7 @@ conn = sqlite3.connect("chat_app.db")
 cur = conn.cursor()
 
 """Task 1: Nájsť používateľov, ktorí poslali správy do konkrétnej miestnosti (napr. room1)."""
+print("Task 1:")
 cur.execute("""
 SELECT DISTINCT users.username
 FROM users
@@ -13,8 +14,16 @@ WHERE rooms.name = 'room1';
 """)
 
 """Task 2: Počítať, koľko rôznych používateľov poslalo správy do jednotlivých miestností."""
+print("Task 2:")
+cur.execute("""
+SELECT rooms.name, COUNT(DISTINCT messages.user_id)
+FROM messages
+JOIN rooms ON messages.room_id = rooms.id
+GROUP BY rooms.name;
+""")
 
 """Task 3: Nájsť miestnosti, do ktorých konkrétny používateľ (napr. user2) poslal správy."""
+
 
 """Task 4: Zobraziť počet správ, ktoré poslal každý používateľ."""
 
